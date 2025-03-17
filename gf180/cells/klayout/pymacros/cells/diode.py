891660ed98c1a24671d03fb4a312306306d2dfd5
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-03-16 17:10:59 -0700

    Update gf180/cells/klayout/pymacros/cells/diode.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- gf180/cells/klayout/pymacros/cells/diode.py
+++ gf180/cells/klayout/pymacros/cells/diode.py
@@ -394,7 +394,7 @@ class diode_dw2ps(pya.PCellDeclarationHelper):
 
     def display_text_impl(self) -> str:
         # Provide a descriptive text for the cell
-        return "diode_dw2ps(L=" + (f"{self.la:.3f}") + ",W=" + (f"{self.wa:.3f}") + ")"
+        return f"diode_dw2ps(L={self.la:.3f},W=" + f"{self.wa:.3f}" + ")"
 
     def coerce_parameters_impl(self) -> None:
         # We employ coerce_parameters_impl to decide whether the handle or the numeric parameter has changed.
